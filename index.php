14441be12e2f8a833186e42578246c96c8f0091c
qTemap <artemka_relit@mail.ru>
2016-08-01 16:21:22 +0300

    com1

--- index.php
+++ index.php
@@ -62,11 +62,10 @@ $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 
 
-	$result = mysql_query('select * from SELECT * FROM heroku_a4a500673f4ced6.admin ',$conn);
+	$result = mysql_query('select * from SELECT * FROM heroku_a4a500673f4ced6.massage ');
 
 	while($row = mysql_fetch_assoc($result)) {
-		echo $row[0];
-		echo $row['login'];
+		echo $row['name'];
 	}
 
 ?>
